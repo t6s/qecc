c1bae86189b19c8c42c98d2d6fc23d1ad4e49447
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2021-10-28 22:29:50 +0900

    make lens an explicit record

--- lens.v
+++ lens.v
@@ -7,16 +7,17 @@ Unset Printing Implicit Defensive.
 Section lens.
 Variables (T : Type) (n m : nat).
 
-Definition lens := {s : m.-tuple 'I_n | uniq s}.
+Record lens := mkLens {lens_t :> m.-tuple 'I_n ; lens_uniq : uniq lens_t}.
+Canonical len_subType := Eval hnf in [subType for lens_t].
 
 Variables (l : lens) (f : m.-tuple T -> m.-tuple T).
 
-Definition extract (t : n.-tuple T) := [tuple tnth t (tnth (val l) i) | i < m].
+Definition extract (t : n.-tuple T) := [tuple tnth t (tnth l i) | i < m].
 Definition inject (t : n.-tuple T) (t' : m.-tuple T) :=
-  [tuple nth (tnth t i) t' (index i (val l)) | i < n].
+  [tuple nth (tnth t i) t' (index i l) | i < n].
 Definition focus (t : n.-tuple T) := inject t (f (extract t)).
 
-Lemma focus_out t i : i \notin (val l) -> tnth (focus t) i = tnth t i.
+Lemma focus_out t i : i \notin val l -> tnth (focus t) i = tnth t i.
 Proof.
 move=> Hi.
 by rewrite tnth_mktuple nth_default // memNindex // !size_tuple.
@@ -25,7 +26,7 @@ Qed.
 Lemma focus_in t : extract (focus t) = f (extract t).
 Proof.
 apply eq_from_tnth => i.
-rewrite !tnth_mktuple [RHS](tnth_nth (tnth t (tnth (val l) i))).
+rewrite !tnth_mktuple [RHS](tnth_nth (tnth t (tnth l i))).
 case: l => /= s Hu.
 by rewrite index_uniq // size_tuple.
 Qed.
@@ -34,7 +35,7 @@ Lemma inject_extract t : inject t (extract t) = t.
 Proof.
 apply eq_from_tnth => i.
 rewrite !tnth_mktuple.
-case/boolP: (index i (val l) < m) => Hi.
+case/boolP: (index i l < m) => Hi.
   rewrite (_ : index _ _ = Ordinal Hi) // -tnth_nth tnth_mktuple.
   by rewrite (tnth_nth i) nth_index // -index_mem size_tuple.
 by rewrite nth_default // leqNgt size_tuple.
@@ -61,13 +62,13 @@ Section lens_comp.
 Variables (n m p : nat) (l1 : lens n m) (l2 : lens m p).
 
 Definition lens_comp : lens n p.
-exists (extract l2 (val l1)).
+exists (extract l2 l1).
 abstract (case: l1 l2 => l1' Hl1 [l2' Hl2] /=;
           rewrite map_inj_uniq ?enum_uniq // => i j /tnth_inj /tnth_inj; exact).
 Defined.
 
-Lemma index_lens_comp i (H : index i (val l1) < m) :
-  index i (val lens_comp) = index (Ordinal H) (val l2).
+Lemma index_lens_comp i (H : index i l1 < m) :
+  index i lens_comp = index (Ordinal H) l2.
 Proof.
 rewrite /=.
 move: l1 l2 H => [l1' Hl1'] [l2' Hl2'] /= H.
@@ -97,7 +98,7 @@ rewrite nth_default; last first.
   rewrite -index_mem -leqNgt size_tuple in Hl1.
   by rewrite size_tuple.
 rewrite nth_default // leqNgt size_tuple.
-rewrite -[X in _ < X](size_tuple (val lens_comp)) index_mem.
+rewrite -[X in _ < X](size_tuple lens_comp) index_mem.
 apply: contra Hl1 => /mapP [j Hj] ->.
 by rewrite mem_tnth.
 Qed.
