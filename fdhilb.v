a3605601987da45a724fe557b3d93832fc7ada0f
Takafumi Saikawa <tscompor@gmail.com>
2022-03-07 17:17:14 +0900

    sanitize fdhilb.v

--- fdhilb.v
+++ fdhilb.v
@@ -60,114 +60,9 @@ Module Exports.
 Notation hilbertType := t.
 Coercion base2 : class_of >-> GRing.Lmodule.class_of.
 Coercion car : hilbertType >-> Sortclass.
-
-
 End Exports.
 End Hilbert.
 Export Hilbert.Exports.
-
-(*** ffun_normedModType ***)
-
-Section FinFunNormedMod.
-Variable K : numDomainType.
-Variable aT : finType.
-Variable rT : normedModType K.
-Local Notation fT := {ffun aT -> rT}.
-
-Check [lmodType _ of fT].
-Fail Check [normedZmodType _ of fT].
-
-Implicit Types (f g : fT).
-
-Definition ffun_norm f : K := \sum_(i in aT) `| f i |.
-
-Lemma ler_ffun_norm_add f g : ffun_norm (f + g) <= ffun_norm f + ffun_norm g.
-Proof.
-rewrite /ffun_norm -big_split /=; apply ler_sum => i _.
-apply/le_trans: (ler_norm_add (f i) (g i)).
-by rewrite ffunE le_eqVlt eqxx.
-Qed.
-Lemma ffun_norm_eq0 f : ffun_norm f = 0 -> f = 0.
-Proof.
-rewrite /ffun_norm => /psumr_eq0P H.
-apply/ffunP => i; rewrite ffunE.
-by apply/normr0_eq0/H.
-Qed.
-Lemma ffun_norm_natmul f k : ffun_norm (f *+ k) = (ffun_norm f) *+ k.
-Proof.
-by rewrite /ffun_norm -sumrMnl; under eq_bigr do rewrite ffunMnE normrMn.
-Qed.
-Lemma ffun_normN f : ffun_norm (- f) = ffun_norm f.
-Proof. by rewrite /ffun_norm; under eq_bigr do rewrite ffunE normrN. Qed.
-
-Definition ffun_normedZmodMixin :=
-  @Num.NormedMixin _ _ _ ffun_norm ler_ffun_norm_add
-    ffun_norm_eq0 ffun_norm_natmul ffun_normN.
-
-Canonical ffun_normedZmodType := NormedZmodType K fT ffun_normedZmodMixin.
-
-Check [normedZmodType _ of fT].
-Fail Check [pseudoMetricNormedZmodType _ of fT].
-Fail Check [pseudoMetricType _ of fT].
-Fail Check [uniformType of fT].
-Fail Check [filteredType _ of fT].
-Fail Check [pointedType _ of fT].
-
-Program Definition ffun_PointedMixin : @Pointed.point_of fT
-Program Definition ffun_FilteredMixin : @Filtered.nbhs_of fT
-
-
-Program Definition ffun_UniformMixin : @Uniform.mixin_of fT nbhs := Uniform.Mixin _ _.
-
-Program Definition ffun_PseudoMetricMixin : @PseudoMetric.mixin_of K fT entourage := PseudoMetric.Mixin _ _ _ _.
-
-Lemma ffun_norm_ball :
-  @ball _ [pseudoMetricType K of fT] = ball_ (fun f => `| f |).
-
-Definition ffun_PseudoMetricNormedZmodMixin :=
-
-Definition matrix_PseudoMetricNormedZmodMixin :=
-  PseudoMetricNormedZmodule.Mixin mx_norm_ball.
-Canonical matrix_pseudoMetricNormedZmodType :=
-  PseudoMetricNormedZmodType K 'M[K]_(m.+1, n.+1) matrix_PseudoMetricNormedZmodMixin.
-
-
-
-Fail Check [normedModType _ of fT].
-
-Lemma ffun_normZ (l : K) f : `| l *: f | = `| l | * `| f |.
-Proof.
-rewrite {1 3}/Num.Def.normr /= /ffun_norm /= big_distrr /=.
-by under eq_bigr do rewrite ffunE normmZ.
-Qed.
-
-Definition ffun_NormedModMixin := NormedModMixin ffun_normZ.
-Canonical ffun_normedModType :=
-  NormedModType K fT ffun_NormedModMixin.
-
-End matrix_NormedModule.
-
-Implicit Types f g : {ffun aT -> rT}.
-
-Definition ffun_scale k f := [ffun a => k *: f a].
-
-Fact ffun_scaleA k1 k2 f : 
-  ffun_scale k1 (ffun_scale k2 f) = ffun_scale (k1 * k2) f.
-Proof. by apply/ffunP=> a; rewrite !ffunE scalerA. Qed.
-Fact ffun_scale1 : left_id 1 ffun_scale.
-Proof. by move=> f; apply/ffunP=> a; rewrite !ffunE scale1r. Qed.
-Fact ffun_scale_addr k : {morph (ffun_scale k) : x y / x + y}.
-Proof. by move=> f g; apply/ffunP=> a; rewrite !ffunE scalerDr. Qed.
-Fact ffun_scale_addl u : {morph (ffun_scale)^~ u : k1 k2 / k1 + k2}.
-Proof. by move=> k1 k2; apply/ffunP=> a; rewrite !ffunE scalerDl. Qed.
-
-Definition ffun_lmodMixin := 
-  LmodMixin ffun_scaleA ffun_scale1 ffun_scale_addr ffun_scale_addl.
-Canonical ffun_lmodType :=
-  Eval hnf in LmodType R {ffun aT -> rT} ffun_lmodMixin.
-
-End FinFunNormedMod.
-
 
 (*** states as qubit tuples ***)
 Section nvect.
@@ -177,13 +72,6 @@ Variable n : nat.
 Variable I : finType.
 Definition nvect := {ffun n.-tuple I -> M}.
       
-Check [lmodType _ of nvect].
-About ffun_lmodType.
-
-
-Check [normedModType _ of nvect].
-
-
 (*** Complex conjugate transpose and complex inner product ***)
 Fact admx_key : unit. Proof. by []. Qed.
 Definition admx m n (A : 'M[C]_(m,n)) := \matrix[admx_key]_(i, j) (A j i)^*.
@@ -193,7 +81,6 @@ Definition inner_product n (u v : 'cV[C]_n) : C :=  (u ^† *m v) 0 0.
 Notation "u ・ v" := (inner_product u v) : complex_scope.
 
 (* Definition Hermitian n (A : 'M[C]_n) := admx A = A. *)
-
 
 (*** bra and ket ***)
 
@@ -244,14 +131,15 @@ Notation "∣ x1 , .. , xn ⟩" := [tuple of ∣x1⟩ :: .. [:: ∣xn⟩ ] ..] :
 Check ∣ 0, 1, 0 ⟩ : 3.-tuple ket_type.
 Fail Check ∣ 0, 1, 0 ⟩ : 3.-tuple qubit.
 
-
-(*** ***)
+End nvect.
 
 
 (*** Unitary transformation ***)
 
 Module Unitary.
 Section ClassDef.
+Notation "u ・ v" := (inner_product u v) : complex_scope.
+
 (* Variable (A B : Hilbert C).*)
 Variable n : nat.
 Local Notation A := 'cV[C]_n (only parsing).
@@ -297,6 +185,3 @@ End Exports.
 
 End Unitary.
 Include Unitary.Exports.
-
-
-(*** ***)
