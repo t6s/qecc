ac0bfdd9d276860e8bc0809fe51c6b452275541d
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2024-01-22 17:27:03 +0900

    focusds_applyU

--- density.v
+++ density.v
@@ -98,19 +98,46 @@ rewrite (bigD1 (extract (lensC l) v)) //= eqxx scale1r big1 ?addr0 // => vj Hj.
 by rewrite eq_sym (negbTE Hj) scale0r conjc0 scale0r.
 Qed.
 
+Section dptranspose.
+Variables (T : lmodType C) (m n : nat).
+Definition dptranspose (M : T ^^ n ^^ m) : T ^^ m ^^ n :=
+  [ffun vi => [ffun vj => M vj vi]].
+Lemma dptranspose_is_linear : linear dptranspose.
+Proof. by move=> x y z; apply/ffunP=> vi; apply/ffunP=> vj; rewrite !ffunE. Qed.
+Canonical dptranspose_lin := Linear dptranspose_is_linear.
+End dptranspose.
+
+Lemma dptransposeK T m n : cancel (@dptranspose T m n) (@dptranspose T _ _).
+Proof. by move=> x; apply/ffunP=> v; apply/ffunP=> w; rewrite !ffunE. Qed.
+
 Section curryds.
 Variables (T : lmodType C) (n m : nat) (l : lens n m).
 
-Definition curryds (M : dsquare T n) : dsquare (dsquare T (n-m)) m :=
+Definition curryds : dsquare T n -> dsquare (dsquare T (n-m)) m :=
+  dpmap (@dptranspose _ (n-m) m) \o dpmap (dpmap (curry dI l)) \o curry dI l.
+Canonical curryds_lin := [linear of curryds].
+
+Lemma currydsE M :
+  curryds M =
   [ffun v : m.-tuple I =>
    [ffun v' : m.-tuple I =>
     [ffun w : (n-m).-tuple I =>
      [ffun w' : (n-m).-tuple I => M (merge dI l v w) (merge dI l v' w')]]]].
+Proof.
+apply/ffunP=> v; apply/ffunP=> v'; apply/ffunP=> w; apply/ffunP=> w'.
+by rewrite !ffunE.
+Qed.
 
-Definition uncurryds (M : dsquare (dsquare T (n-m)) m) : dsquare T n :=
+Definition uncurryds : dsquare (dsquare T (n-m)) m -> dsquare T n :=
+  uncurry l \o dpmap (dpmap (uncurry l)) \o dpmap (@dptranspose _ m (n-m)).
+Canonical uncurryds_lin := [linear of uncurryds].
+
+Definition uncurrydsE M :
+  uncurryds M =
   [ffun v : n.-tuple I =>
    [ffun w : n.-tuple I =>
     M (extract l v) (extract l w) (extract (lensC l) v) (extract (lensC l) w)]].
+Proof. by apply/ffunP=> v; apply/ffunP=> w; rewrite !ffunE. Qed.
 
 Lemma uncurrydsK : cancel uncurryds curryds.
 Proof.
@@ -122,21 +149,62 @@ Lemma currydsK : cancel curryds uncurryds.
 Proof.
 move=> M; apply/ffunP=> v; apply/ffunP=> w; by rewrite !ffunE !merge_extract.
 Qed.
-
-Lemma curryds_is_linear : linear curryds.
-Proof. move=> x y z; do 4 apply/ffunP => ?; by rewrite !ffunE. Qed.
-Canonical curryds_lin := Linear curryds_is_linear.
-
-Lemma uncurryds_is_linear : linear uncurryds.
-Proof. move=> x y z; do 2 apply/ffunP => ?; by rewrite !ffunE. Qed.
-Canonical uncurryds_lin := Linear uncurryds_is_linear.
 End curryds.
 
+Lemma eq_dpmap (T1 T2 : lmodType C) m (f g : T1 -> T2) :
+  f =1 g -> @dpmap I m T1 T2 f =1 dpmap g.
+Proof. by move=> fg X; apply/ffunP => v; rewrite !ffunE fg. Qed.
+
+Lemma dpmap_compose (T1 T2 T3 : lmodType C) m (f : T1 -> T2) (g : T2 -> T3) X :
+  @dpmap I m _ _ (g \o f) X = dpmap g (dpmap f X).
+Proof. by apply/ffunP=> v; rewrite !ffunE. Qed.
+
+Lemma dpmap_dptranspose (T : lmodType C) m n p (f : mor m p) X :
+  dpmap (f T) (@dptranspose T m n X) = dptranspose (f _ X).
+Proof.
+apply/ffunP=> v; apply/ffunP=> w /=.
+rewrite ffunE.
+move/naturalityP: (morN f) => [M Hf].
+rewrite !Hf !ffunE !mxmorE sum_ffunE.
+apply eq_bigr => vj _.
+by rewrite !ffunE.
+Qed.
+
+Lemma dptranspose_dpmap (T : lmodType C) m n p (f : mor m p) X :
+  @dptranspose T n p (dpmap (f T) X) = f _ (dptranspose X).
+Proof. by rewrite -[in LHS](dptransposeK X) dpmap_dptranspose dptransposeK. Qed.
+
+Lemma uncurry_dpmap (T : lmodType C) n m p q  (l : lens n m) (f : mor p q)
+      (X : T ^^ p ^^ (n-m) ^^ m) :
+  uncurry l (dpmap (dpmap (f T)) X) = dpmap (f T) (uncurry l X).
+Proof. by apply/ffunP=> v; rewrite !ffunE. Qed.
+
+Print dptranspose.
 (* Application of adjunction commutes with focus *)
 Lemma focusds_applyU (T : lmodType C) n m (l : lens n m) (f : endo m)
       (M : dsquare T n) :
   applyU (focus l f) M = uncurryds l (applyU f (curryds l M)).
 Proof.
-Abort.
+rewrite /applyU /curryds /uncurryds.
+rewrite -(eq_dpmap (@focus_hconj_mor _ _ l f T)).
+rewrite -2!morN.
+set fc := hconj_mor f.
+rewrite /=.
+rewrite -(dpmap_compose (fc _)).
+rewrite -(eq_dpmap (dpmap_dptranspose fc)).
+rewrite (_ : (fun X : (ffun_lmodType (tuple_finType (n - m) I) T ^^ (n - m)) ^^ m => dpmap _ (dptranspose X)) = dpmap (fc (ffun_lmodType (tuple_finType (n - m) I) T)) \o dptranspose (n:=n-m)) //.
+rewrite [in RHS]dpmap_compose.
+rewrite -(dpmap_compose (dptranspose (n:=m))).
+rewrite (eq_dpmap (@dptransposeK _ (n-m) m)).
+rewrite -(dpmap_compose _ id).
+rewrite (_ : id \o _ = dpmap (curry dI l)) //.
+rewrite -dpmap_compose.
+rewrite -(eq_dpmap (dpmap_compose (curry dI l) (fc _))).
+rewrite -dpmap_compose.
+rewrite -(eq_dpmap (dpmap_compose (fc _ \o curry dI l) (uncurry l))).
+rewrite -[X in uncurry l (dpmap (dpmap X) _)](focusE dI l fc _).
+rewrite uncurry_dpmap.
+by rewrite [in X in dpmap _ X]focusE.
+Qed.
 
 End density.
